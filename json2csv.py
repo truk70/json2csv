c161609c6ca06367776a542ec8784d1d376991ba
Kurt Gerber <kurtgerber70@gmail.com>
2021-03-29 17:11:08 +0200

    Generalized the script so the functions can be imported by another script. E.g. one that reads from an API directly

--- json2csv.py
+++ json2csv.py
@@ -4,32 +4,29 @@
 # for more information, how to use it, execute the following:
 # python3 json2csv.py -h   
 # Author: kurtgerber70@gmail.com
-# Date: 20.03.2021
+# Date: 29.03.2021
 # 
 
 import json 
 import csv 
-import sys
-import os.path  
-import argparse
 
-def list_generator(indict, pre=None):
+def __list_generator(indict, pre=None):
     pre = pre[:] if pre else []
     if isinstance(indict, dict):
         for key, value in indict.items():
             if isinstance(value, dict):
-                for d in list_generator(value, pre + [key]):
+                for d in __list_generator(value, pre + [key]):
                     yield d
             elif isinstance(value, list) or isinstance(value, tuple):
                 for v in value:
-                    for d in list_generator(v, pre + [key]):
+                    for d in __list_generator(v, pre + [key]):
                         yield d
             else:
                 yield pre + [key, value]
     else:
         yield pre + [indict]
 
-def dict_generator(inlist):
+def __dict_generator(inlist):
     dict_out = {}
     for item in inlist:
         myval = item.pop()
@@ -42,60 +39,76 @@ def dict_generator(inlist):
         
     return(dict_out)
 
-parser = argparse.ArgumentParser()
-   
-parser.add_argument('jsonpath', help="defines json path to the list to parse. Example: data.deals")
-parser.add_argument('file', help="path to json file to parse")
+def json2csv(data,csv_file):
+    # data: dictionary loaded with json.load()
+    # csv_file: file path and name to which the csv output will be written
 
-args = parser.parse_args()
+    # now we will open a file for writing 
+    data_file = open(csv_file, 'w',newline='') 
+  
+    # create the csv writer object 
+    csv_writer = csv.writer(data_file) 
 
-if not os.path.isfile(args.file):
-    print("Needs a valid json file as argument")
-    parser.print_help()
-    sys.exit(1)
+    # Counter variable used for writing  
+    # headers to the CSV file 
+    count = 0
 
-# Opening JSON file and loading the data 
-# into the variable data 
-with open(args.file) as json_file: 
-    data = json.load(json_file)
+    for item in data: 
+        data_dict_flat = __dict_generator(__list_generator(item))
+        if count == 0:
+            #Writing headers of CSV file 
+            header = data_dict_flat.keys() 
+            csv_writer.writerow(header) 
+        else:  
+            # Writing data of CSV file 
+            csv_writer.writerow(data_dict_flat.values()) 
+        count =+ 1 
+  
+    data_file.close()
 
-# converting path string into a list
-path_list = args.jsonpath.split(".")
 
+if __name__ == "__main__":
+    import sys
+    import os.path
+    import argparse
 
-# for each element in path_list, slice the data a bit more
-for k in path_list:
-    try:
-        data = data[k]
-    except:
-        print("Path element %s not found", k)
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('jsonpath', help="defines json path to the list to parse. Example: data.deals")
+    parser.add_argument('file', help="path to json file to parse")
+
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.file):
+        print("Needs a valid json file as argument")
+        parser.print_help()
         sys.exit(1)
 
+    # Opening JSON file and loading the data 
+    # into the variable data 
+    with open(args.file) as json_file: 
+        data = json.load(json_file)
 
-mypath,myfile = os.path.split(args.file)
-base_file,ext = os.path.splitext(myfile)
-out_file = '.'.join((base_file,'csv')) 
-csv_file = os.path.join(mypath,out_file) 
+    # converting path string into a list
+    path_list = args.jsonpath.split(".")
 
-# now we will open a file for writing 
-data_file = open(csv_file, 'w',newline='') 
-  
-# create the csv writer object 
-csv_writer = csv.writer(data_file) 
-  
-# Counter variable used for writing  
-# headers to the CSV file 
-count = 0
 
-for item in data: 
-    data_dict_flat = dict_generator(list_generator(item))
-    if count == 0:
-        #Writing headers of CSV file 
-        header = data_dict_flat.keys() 
-        csv_writer.writerow(header) 
-    else:  
-        # Writing data of CSV file 
-        csv_writer.writerow(data_dict_flat.values()) 
-    count =+ 1 
+    # for each element in path_list, slice the data a bit more
+    for k in path_list:
+        try:
+            data = data[k]
+        except:
+            print("Path element %s not found", k)
+            sys.exit(1)
+
+    #Create the output file name based on the input file name
+    mypath,myfile = os.path.split(args.file)
+    base_file,ext = os.path.splitext(myfile)
+    out_file = '.'.join((base_file,'csv')) 
+    csv_file = os.path.join(mypath,out_file) 
+
+    # Execute the the transformation
+    json2csv(data,csv_file)
+
   
-data_file.close() 
+ 
